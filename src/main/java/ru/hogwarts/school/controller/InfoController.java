d1ac1db11dbe44d9e1fc84bfe926b1036d3f6c8e
semenikhin <garry@nextmail.ru>
2023-07-19 16:08:22 +0300

    First commit

---
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -0,0 +1,21 @@
+package ru.hogwarts.school.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.hogwarts.school.service.InfoService;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/info")
+public class InfoController {
+    private final InfoService infoService;
+
+    @GetMapping("/port")
+    public ResponseEntity<Integer> getPort() {
+        return ResponseEntity.ok(infoService.getPort());
+    }
+
+}
---
+++ src/main/java/ru/hogwarts/school/service/InfoService.java
@@ -0,0 +1,15 @@
+package ru.hogwarts.school.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class InfoService {
+    private final ServerProperties serverProperties;
+
+    public Integer getPort() {
+        return serverProperties.getPort();
+    }
+}
